18c8d10365408a407fb23e63124b07250a19039c
ColetonS <coleton.m.smith@gmail.com>
2019-08-14 14:44:04 -0600

    color gradient for landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,59 +1,74 @@
 import React, { Component } from "react";
-import axios from 'axios'
-import {connect} from 'react-redux'
-import {setUser} from '../../ducks/reducer'
-import './Landing.css'
+import axios from "axios";
+import { connect } from "react-redux";
+import { setUser } from "../../ducks/reducer";
+import "./Landing.scss";
 
 class Landing extends Component {
   state = {
-      usernameInput: '',
-      passwordInput: ''
+    usernameInput: "",
+    passwordInput: ""
   };
 
   handleChange(e, key) {
     // console.log(this.state)
     this.setState({
-        [key]: e.target.value
-    })
+      [key]: e.target.value
+    });
   }
 
   registerUser = () => {
-      const {usernameInput:username, passwordInput:password} = this.state
-      axios.post('/api/auth/register', {username, password}).then(res => {
-          const {user_id, username, user_image} = res.data.user
-          this.props.setUser({user_id, username, user_image})
-          this.props.history.push('/dashboard')
+    const { usernameInput: username, passwordInput: password } = this.state;
+    axios
+      .post("/api/auth/register", { username, password })
+      .then(res => {
+        const { user_id, username, user_image } = res.data.user;
+        this.props.setUser({ user_id, username, user_image });
+        this.props.history.push("/dashboard");
       })
       .catch(() => {
-          alert('Username aready in use')
-      })
-  }
+        alert("Username aready in use");
+      });
+  };
 
   loginUser = () => {
-      const {usernameInput:username, passwordInput:password} = this.state
-      axios.post('/api/auth/login', {username, password}).then(res => {
-        // console.log(res.data.user)
-          const {user_id, username, user_image} = res.data.user
-          // console.log({user_id, username, user_image})
-          this.props.setUser({user_id, username, user_image})
-          this.props.history.push('/dashboard')
-      })
-  }
+    const { usernameInput: username, passwordInput: password } = this.state;
+    axios.post("/api/auth/login", { username, password }).then(res => {
+      // console.log(res.data.user)
+      const { user_id, username, user_image } = res.data.user;
+      // console.log({user_id, username, user_image})
+      this.props.setUser({ user_id, username, user_image });
+      this.props.history.push("/dashboard");
+    });
+  };
 
   render() {
-    console.log(this.props.history)
+    console.log(this.props.history);
     return (
-      <div className='landing'>
-        <div className="logo">
-          <div>
-            :)
+      <div className="landing">
+        <div className="input-container">
+          <div className="logo">
+            <div>Logo</div>
           </div>
-        </div>
-        <div className='input-container'>
-          <input onChange={e => this.handleChange(e, 'usernameInput')} type='text' placeholder='username'/>
-          <input onChange={e => this.handleChange(e, 'passwordInput')} type='password' placeholder='password'/>
-          <button onClick={this.loginUser}>Login</button>
-          <button onClick={this.registerUser}>Register</button>
+          <div className="title">App Name</div>
+          <div>
+            <p>Username:</p>
+            <input
+              onChange={e => this.handleChange(e, "usernameInput")}
+              type="text"
+            />
+          </div>
+          <div>
+            <p>Password</p>
+            <input
+              onChange={e => this.handleChange(e, "passwordInput")}
+              type="password"
+            />
+          </div>
+          <div className='buttons'>
+            <button onClick={this.loginUser}>Login</button>
+            <button onClick={this.registerUser}>Register</button>
+          </div>
         </div>
       </div>
     );
@@ -61,8 +76,11 @@ class Landing extends Component {
 }
 
 function mapStateToProps(reduxState) {
-  const {user_id, username, user_image} = reduxState
-  return {user_id, username, user_image}
+  const { user_id, username, user_image } = reduxState;
+  return { user_id, username, user_image };
 }
 
-export default connect(mapStateToProps, {setUser})(Landing)
+export default connect(
+  mapStateToProps,
+  { setUser }
+)(Landing);
