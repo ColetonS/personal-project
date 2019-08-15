1227cc080bd788fd7c29c644a6da9b7db24d5e5f
ColetonS <coleton.m.smith@gmail.com>
2019-08-15 17:37:19 -0600

    set up imitations on back end

--- db/get_all_imitations.sql
+++ db/get_all_imitations.sql
@@ -1,2 +1,3 @@
-SELECT * FROM imitations
-WHERE user_id = $1;
+SELECT e.excerpt_text, e.excerpt_author, e.excerpt_narrative, e.excerpt_image, i.completed_imitation_id, i.user_id, i.completed_imitation_text FROM imitations i
+JOIN excerpts e ON i.excerpt_id = e.excerpt_id
+WHERE i.user_id = $1;
