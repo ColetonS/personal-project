689cde7097cb08e5da81f114e96411182062c512
ColetonS <coleton.m.smith@gmail.com>
2019-08-23 13:14:16 -0600

    contact page responsive

--- src/components/Email/Email.js
+++ src/components/Email/Email.js
@@ -41,7 +41,7 @@ export default class Email extends Component {
       <div className="email-main">
         <div className='header-and-input-container'>
           <div className="email-header">
-            <h1>Email Me</h1>
+            <h1>Contact Me</h1>
           </div>
           <div className="email-inputs">
             <input
