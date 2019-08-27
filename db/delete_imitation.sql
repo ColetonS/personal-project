6a02ee858ed5485595f6ccb15dbbeb93275d467e
ColetonS <coleton.m.smith@gmail.com>
2019-08-27 16:34:48 -0600

    background on landing page

--- db/delete_imitation.sql
+++ db/delete_imitation.sql
@@ -3,4 +3,5 @@ WHERE completed_imitation_id = $1;
 
 SELECT e.excerpt_text, e.excerpt_author, e.excerpt_narrative, e.excerpt_image, i.completed_imitation_id, i.user_id, i.completed_imitation_text FROM imitations i
 JOIN excerpts e ON i.excerpt_id = e.excerpt_id
-WHERE i.user_id = $2;
+WHERE i.user_id = $2
+ORDER BY i.completed_imitation_id DESC;
